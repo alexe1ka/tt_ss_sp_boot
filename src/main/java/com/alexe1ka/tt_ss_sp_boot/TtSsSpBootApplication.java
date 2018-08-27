3549b7d7d3b81971f0b33d8600a9c3fc4a0c757f
alexe1ka <leshka_09@bk.ru>
2018-08-27 19:54:54 +0300

    write app.prop

---
+++ src/main/java/com/alexe1ka/tt_ss_sp_boot/TtSsSpBootApplication.java
@@ -0,0 +1,12 @@
+package com.alexe1ka.tt_ss_sp_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtSsSpBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtSsSpBootApplication.class, args);
+	}
+}
